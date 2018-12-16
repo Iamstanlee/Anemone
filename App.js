56270fe958160527ae09c9c07a4ce20d70a9fa18
Ananya <ananyacleetus@gmail.com>
2018-12-16 11:14:08 -0500

    bottom tabs navigation

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import AppNavigator from './AppNavigator';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <AppNavigator/>
+      <View>
+
+        <TouchableOpacity
+          style={styles.item}
+          onPress={() => this.props.navigation.push('BottomTabs')}
+        >
+          <Text>~{"\n"}~{"\n"}~{"\n"}Bottom tabs</Text>
+        </TouchableOpacity>
+        <AppNavigator/>
+      </View>
     );
   }
 }
+
+const styles = {
+  item: {
+    padding: 16,
+    backgroundColor: '#fff',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: '#eee',
+  },
+};
--- AppNavigator.js
+++ AppNavigator.js
@@ -1,12 +1,23 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import HomeScreen from './screens/HomeScreen';
 import StartScreen from './screens/StartScreen';
 import NextScreen from './screens/NextScreen';
+import BottomTabs from './src/BottomTabs';
 
 const AppNavigator = createStackNavigator({
-  HomeScreen: { screen: HomeScreen},
-  StartScreen: {screen: StartScreen},
-  NextScreen: {screen: NextScreen}
+  // HomeScreen: {screen: HomeScreen},
+  // StartScreen: {screen: StartScreen},
+  // NextScreen: {screen: NextScreen}
+  HomeScreen: {
+   screen: HomeScreen,
+   navigationOptions: { title: 'Examples' },
+ },
+ BottomTabs: {
+   screen: BottomTabs,
+   navigationOptions: { title: 'Bottom tabs' },
+ },
 });
 const App = createAppContainer(AppNavigator);
 
