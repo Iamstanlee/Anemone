08784ab0344a24349791c0c5de79224a6d101923
Ananya <ananyacleetus@gmail.com>
2018-12-16 20:01:26 -0500

    navbar icons

--- AppNavigator.js
+++ AppNavigator.js
@@ -1,34 +1,38 @@
 import React from 'react';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
-import {Icon, View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import {MaterialIcons } from 'react-native-vector-icons';
+import {View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import HomeScreen from './screens/HomeScreen';
 import CrisisPlan from './screens/CrisisPlan';
 import EmergencyResources from './screens/EmergencyResources';
 import CopingSkills from './screens/CopingSkills';
 
-const tabBarIcon = name => ({ tintColor, horizontal }) => (
-  <MaterialIcons name={name} color={tintColor} size={horizontal ? 17 : 24} />
-);
+// const tabBarIcon = name => ({focused, horizontal, tintColor }) => (
+//   <MaterialIcons name={name} color={focused ? tintColor: tintColor} size={horizontal ? 17 : 24} />
+// );
 
 const TabNavigator = createMaterialBottomTabNavigator({
  CrisisPlan: {
    screen: CrisisPlan,
    navigationOptions: { title: 'Crisis Plan',
    tabBarColor: '#AF7B93',
-   tabBarIcon: tabBarIcon('photo-album'),
+   tabBarIcon: <Icon size={24} color="white" name="star" />,
   },
    },
  EmergencyResources: {
    screen: EmergencyResources,
    navigationOptions: { title: 'Emergency Resources',
-   tabBarColor: '#7BD2D8'},
+   tabBarColor: '#7BD2D8',
+   tabBarIcon: <Icon size={24} color="white" name="phone" />,
+},
  },
  CopingSkills: {
    screen: CopingSkills,
    navigationOptions: { title: 'Coping Skills',
-   tabBarColor: '#E6EFBF'},
+   tabBarColor: '#B6D332',
+   tabBarIcon: <Icon size={24} color="white" name="wb-sunny" />,
+},
  },
 },
  {
