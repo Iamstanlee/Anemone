cf33d174ec50d746d8376f1ab09a4e06e234b376
Ananya <ananyacleetus@gmail.com>
2018-12-20 16:21:49 -0500

    circular buttons

--- screens/EmergencyResources.js
+++ screens/EmergencyResources.js
@@ -7,7 +7,7 @@
  */
 
 import React from 'react';
-import {Platform, StyleSheet, Text, View, Button, SectionList, TouchableHighlight, Linking, Icon} from 'react-native';
+import {Platform, StyleSheet, Text, View, Button, SectionList, TouchableHighlight, Linking, Icon, Dimensions} from 'react-native';
 import {List, ListItem} from 'react-native-elements';
 import call from 'react-native-phone-call'
 import SendSMS from 'react-native-sms'
@@ -38,16 +38,7 @@ const list = [
     component: () =>
     <View>
     <TouchableHighlight
-    style={{
-      borderWidth:1,
-      borderColor:'rgba(0,0,0,0.2)',
-      alignItems:'center',
-      justifyContent:'center',
-      width:100,
-      height:100,
-      backgroundColor:'#fff',
-      borderRadius:100,
-    }}
+    style={this.buttonStyle(1, 1)}
     onPress={()=> Linking.openURL('https://www.google.com/maps/search/urgent+care/').catch(err => console.error('An error occurred', err))}>
     <View>
     <Text style={styles.buttonText}>Urgent Care</Text>
@@ -63,16 +54,7 @@ const list = [
     component: () =>
     <View>
     <TouchableHighlight
-    style={{
-      borderWidth:1,
-      borderColor:'rgba(0,0,0,0.2)',
-      alignItems:'center',
-      justifyContent:'center',
-      width:100,
-      height:100,
-      backgroundColor:'#fff',
-      borderRadius:100,
-    }}
+    style={this.buttonStyle(1, 2)}
     onPress={()=> Linking.openURL('https://www.google.com/maps/search/hospital/').catch(err => console.error('An error occurred', err))}>
     <View>
     <Text style={styles.buttonText}>Hospitals</Text>
@@ -87,16 +69,7 @@ const list = [
     component: () =>
     <View>
     <TouchableHighlight
-    style={{
-      borderWidth:1,
-      borderColor:'rgba(0,0,0,0.2)',
-      alignItems:'center',
-      justifyContent:'center',
-      width:100,
-      height:100,
-      backgroundColor:'#fff',
-      borderRadius:100,
-    }}
+    style={this.buttonStyle(2, 1)}
     onPress={()=> call(emergencyline).catch(console.error)}>
     <View>
     <Text style={styles.buttonText}>Emergency Hotline</Text>
@@ -111,16 +84,7 @@ const list = [
     component: () =>
     <View>
     <TouchableHighlight
-    style={{
-      borderWidth:1,
-      borderColor:'rgba(0,0,0,0.2)',
-      alignItems:'center',
-      justifyContent:'center',
-      width:100,
-      height:100,
-      backgroundColor:'#fff',
-      borderRadius:100,
-    }}
+    style={this.buttonStyle(2, 2)}
     onPress={()=> call(suicideLine).catch(console.error)}>
     <View>
     <Text style={styles.buttonText}>Suicide Hotline</Text>
@@ -134,16 +98,7 @@ const list = [
     component: () =>
     <View>
     <TouchableHighlight
-    style={{
-      borderWidth:1,
-      borderColor:'rgba(0,0,0,0.2)',
-      alignItems:'center',
-      justifyContent:'center',
-      width:100,
-      height:100,
-      backgroundColor:'#fff',
-      borderRadius:100,
-    }}
+    style={this.buttonStyle(3, 1)}
     onPress={()=> SendSMS.send(suicideTextLine, (completed, cancelled, error) => {console.log('SMS Callback: completed: ' + completed + ' cancelled: ' + cancelled + 'error: ' + error);
 
 	})}>
@@ -153,15 +108,51 @@ const list = [
   </TouchableHighlight>
   </View> ,
   },
+
+  {
+    title: 'Personal Contact',
+    icon: 'flight-takeoff',
+    component: () =>
+    <View>
+    <TouchableHighlight
+    style={this.buttonStyle(3, 3)}
+    //TODO: Set up if in async, call number, else modal for setting contact
+    onPress={()=> call(suicideLine).catch(console.error)}>
+  <View>
+  <Text style={styles.buttonText}>Personal Contact</Text>
+  </View>
+  </TouchableHighlight>
+  </View> ,
+  },
 ]
 
 export default class EmergencyResources extends React.Component{
+
+
 
   // _linkPressed=function(url){
   //   Linking.openURL(url).catch(err => console.error('An error occurred', err));
   //  }
 
   render() {
+    var {height, width} = Dimensions.get('window');
+
+    buttonStyle = function(rNum, cNum) {
+     return {
+       borderWidth:1,
+       borderColor:'rgba(0,0,0,0.2)',
+       alignItems:'center',
+       justifyContent:'center',
+       width:100,
+       height:100,
+       backgroundColor:'#fff',
+       borderRadius:100,
+       top: (rNum*(height))/6,
+       left: (cNum*(width))/6,
+
+     }
+   }
+
     return (
       <View>
     <Text>
@@ -170,6 +161,7 @@ export default class EmergencyResources extends React.Component{
 
 
       <List>
+      <View style={{flexDirection: 'row'}}>
   {
     list.map((item) => (
       <ListItem
@@ -180,6 +172,7 @@ export default class EmergencyResources extends React.Component{
       />
     ))
   }
+  </View>
 </List>
 </View>
 
@@ -188,10 +181,6 @@ export default class EmergencyResources extends React.Component{
 }
 
 const styles = StyleSheet.create({
-  container: {
-   flex: 1,
-   paddingTop: 22
-  },
   sectionHeader: {
     paddingTop: 2,
     paddingLeft: 10,
@@ -200,24 +189,23 @@ const styles = StyleSheet.create({
     fontSize: 14,
     fontWeight: 'bold',
     backgroundColor: 'rgba(247,247,247,1.0)',
-  },
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
-  },
-  linearGradient: {
-    flex: 1,
-    paddingLeft: 15,
-    paddingRight: 15,
-    borderRadius: 5
-  },
+},
   buttonText: {
     fontSize: 15,
-    fontFamily: 'Gill Sans',
+    //fontFamily: 'Gill Sans',
     textAlign: 'center',
     margin: 10,
     color: '#000000',
     backgroundColor: 'transparent',
+  },
+  buttonStyle: {
+    borderWidth:1,
+    borderColor:'rgba(0,0,0,0.2)',
+    alignItems:'center',
+    justifyContent:'center',
+    width:100,
+    height:100,
+    backgroundColor:'#fff',
+    borderRadius:100,
   },
 });
