8a0cbd0350e1b5f9523c29664d6da668c56a52c9
Ananya <ananyacleetus@gmail.com>
2018-12-21 22:37:49 -0500

    modal test

--- screens/CopingSkills.js
+++ screens/CopingSkills.js
@@ -1,14 +1,43 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView, TouchableWithoutFeedback} from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TouchableWithoutFeedback, TouchableHighlight, Modal} from 'react-native';
 import { Card, ListItem, Button, Icon } from 'react-native-elements'
 import * as Animatable from 'react-native-animatable';
 
 export default class CopingSkills extends React.Component {
+
+  state = {
+    modalVisible: false,
+  };
+
+  setModalVisible(visible) {
+    this.setState({modalVisible: visible});
+  }
+
   render() {
     return (
       <View style={styles.container}>
+      <Modal
+          animationType="fade"
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {
+            alert('Modal has been closed.');
+          }}>
+          <View style={{marginTop: 22}}>
+            <View>
+              <Text>Hello World!</Text>
+
+              <TouchableHighlight
+                onPress={() => {
+                  this.setModalVisible(!this.state.modalVisible);
+                }}>
+                <Text>Hide Modal</Text>
+              </TouchableHighlight>
+            </View>
+          </View>
+        </Modal>
       <ScrollView contentContainerStyle={{ paddingVertical: 20 }}>
-      <TouchableWithoutFeedback onPress={() => this.refs.grounding.pulse(200)}>
+      <TouchableWithoutFeedback onPress={() => {this.refs.grounding.pulse(200); this.setModalVisible(true);}}>
       <Animatable.View ref="grounding">
       <Card
       title='Grounding'
